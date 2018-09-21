08cd09bf256f0a8c1465f78a0275db6950b8f21f
Konrad Sedkowski <ksedkowski@gmail.com>
2018-09-21 15:56:04 +0200

    corrected formating and delete jquery script

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
-function Telefon(marka, cena, kolor, ram, rez){
-	this.marka = marka;
-	this.cena = cena;
-	this.kolor = kolor;
+function Telefon(brand, price, color, ram, rez){
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
 	this.ram = ram;
 	this.rez = rez;
 }
 
 Telefon.prototype.printInfo = function() {
-    console.log("Marka telefonu to " + this.marka + ", kolor telefonu " + this.kolor +  
-                ", a cena telefonu: " +  this.cena + ", pamięć ram " + this.ram + ", rodzielczość ekranu " + this.rez + ".");
+      console.log("Marka telefonu " + this.brand
+    			+ ", kolor telefonu " + this.color
+    			+ ", cena telefonu: " +  this.price
+    			+ ", pamięć ram " + this.ram
+    			+ ", rodzielczość ekranu " + this.rez
+    			+ ".");
 }
 
-var Iphone = new Telefon('Iphone I6', '3420zł', 'zloty', '1GB', '728x1024');
-var Samsung = new Telefon('Samsung Galaxy S8', '3299zł', 'czarny', '4GB', '1024x1280');
-var Xiaoami =  new Telefon('Xiaoami Mi8', '599$', 'Biały', '6GB', '2024x4028');
-
+var Iphone = new Telefon('Iphone I6','3420zł', 
+						'zloty', '1GB', '728x1024');
+var Samsung = new Telefon('Samsung Galaxy S8', '3299zł',
+						'czarny', '4GB', '1024x1280');
+var Xiaoami =  new Telefon('Xiaoami Mi8', '599$',
+						'Biały', '6GB', '2024x4028');
 Xiaoami.printInfo();
